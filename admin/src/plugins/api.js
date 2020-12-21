95920c46c232307ea7016271ac8be75683a797fb
Yaroslav <lixometr@mail.ru>
2020-12-21 20:59:37 +0300

    fix api admin

--- admin/src/plugins/api.js
+++ admin/src/plugins/api.js
@@ -2,14 +2,16 @@ import Api from "@/../../common/api_routes/api";
 import ApiRoutes from "@/../../common/api_routes/api_routes";
 import store from "@/store/store";
 import axios from "axios";
+import globalConfig from "@/helpers/globalConfig"
+
 const instance = axios.create()
 instance.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.getters['user/token']}`
     return config
 }
 );
-import globalConfig from "@/helpers/globalConfig"
 
 const baseUrl = globalConfig.api.baseUrl
+console.log('url is here', baseUrl, globalConfig)
 console.log('hey', process.env.NODE_ENV)
 export default new Api(ApiRoutes({ baseUrl }), instance)
