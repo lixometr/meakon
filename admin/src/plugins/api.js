6a289e589b376dcc9f5fde3e0cfa2aad756637ce
Yaroslav <lixometr@mail.ru>
2020-12-21 20:40:07 +0300

    fix api admin

--- admin/src/plugins/api.js
+++ admin/src/plugins/api.js
@@ -2,16 +2,16 @@ import Api from "@/../../common/api_routes/api";
 import ApiRoutes from "@/../../common/api_routes/api_routes";
 import store from "@/store/store";
 import axios from "axios";
-
 const instance = axios.create()
 instance.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.getters['user/token']}`
     return config
 }
 );
-let baseUrl = 'http://localhost:8080'
+let globalConfig = require('../../../common/config/config.development')
 if (process.env.NODE_ENV === 'production') {
-    baseUrl = 'https://api.lootchampion.com'
+    globalConfig = require('../../../common/config/config.production')
 }
+const baseUrl = globalConfig.api.baseUrl
 console.log('hey', process.env.NODE_ENV)
 export default new Api(ApiRoutes({ baseUrl }), instance)
